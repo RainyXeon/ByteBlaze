9b2c4d95fe48885f2b58f5e537e83543e03b7f7f
XeonE52680v3 <minh15052008@gmail.com>
2023-09-19 09:48:23 +0700

    Fix client.lavalink_using no hostname

--- src/events/node/ready.ts
+++ src/events/node/ready.ts
@@ -1,9 +1,4 @@
 import { Manager } from "../../manager.js";
-const regex =
-  /^(wss?|ws?:\/\/)([0-9]{1,3}(?:\.[0-9]{1,3}){3}|[^\/]+):([0-9]{1,5})$/;
-  
- const regex_no_port =
-  /^(wss?|ws?:\/\/)([0-9]{1,3}(?:\.[0-9]{1,3}){3}|[^\/]+)/;
 
 export default async (client: Manager, name: string) => {
   if (client.used_lavalink.length != 0 && client.used_lavalink[0].name == name)
@@ -11,16 +6,12 @@ export default async (client: Manager, name: string) => {
   client.fixing_nodes = false;
 
   client.manager.shoukaku.nodes.forEach((data, index) => {
-	let res
-    res = regex.exec(data["url"]);
-	if (res == null) {
-		res = regex_no_port.exec(data["url"])
-	}
+    const reqUrl = new URL(data["url"], data["url"]);
     client.lavalink_using.push({
-      host: res![2],
-      port: Number(res![3]) | 0,
+      host: reqUrl.hostname,
+      port: Number(reqUrl.port) | 0,
       pass: data["auth"],
-      secure: res![1] == "ws://" ? false : true,
+      secure: reqUrl.protocol == "ws://" ? false : true,
       name: index,
     });
   });
