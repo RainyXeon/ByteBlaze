1314b8150f00e935ad5e00ab3f09e819985df210
RainyXeon / <minh15052008@gmail.com>
2023-12-06 17:03:36 +0700

    Build test

--- build.mjs
+++ build.mjs
@@ -2,16 +2,36 @@
 
 import { spawn } from "node:child_process";
 import archiver from "dir-archiver";
-import { rimraf } from "rimraf";
 import { XMLParser, XMLBuilder } from "fast-xml-parser";
 import fse from "fs-extra";
 import { plsParseArgs } from "plsargs";
+import copydir from 'copy-dir';
+import path from "path"
 const args = plsParseArgs(process.argv.slice(2));
 const parser = new XMLParser();
 const builder = new XMLBuilder();
 const objectDate = Date.now();
 
-const acceptedParams = ["clean", "build"];
+const acceptedParams = ["clean", "build", "build:github"];
+
+const ignored = [
+  "node_modules",
+  ".env",
+  ".eslintrc.cjs",
+  ".gitignore",
+  ".prettierrc.json",
+  "app.yml",
+  ".git",
+  ".cylane",
+  "src",
+  "scripts",
+  "build.mjs",
+  "byteblaze.database.json",
+  "pnpm-lock.yaml",
+  "tsconfig.json",
+  ".github",
+  "out"
+];
 
 function logger(data, type) {
   const text = String(data).replace(/(\r\n|\n|\r)/gm, " || ");
@@ -44,77 +64,129 @@ if (args.get(0) == acceptedParams[0]) {
   process.exit();
 }
 
-// Build
-const child = spawn(/^win/.test(process.platform) ? "npm.cmd" : "npm", [
-  "run",
-  "build:full",
-]);
+console.log(args.get(0), acceptedParams[2])
 
-child.stdout.on("data", (data) => {
-  logger(data, "build");
-});
+if (args.get(0) == acceptedParams[2]) {
+  const child = spawn(/^win/.test(process.platform) ? "npm.cmd" : "npm", [
+    "run",
+    "build:full",
+  ]);
+  
+  child.stdout.on("data", (data) => {
+    logger(data, "build");
+  });
+  
+  child.stderr.on("data", (data) => {
+    logger(data, "build");
+  });
+  
+  child.on("error", (error) => {
+    logger(error.message, "error");
+  });
+  
+  child.on("close", async (code) => {
+    logger(`Build finished with code ${code}`, "build");
+  
+    // Edit manifest
+    const manifestRaw = fse.readFileSync("./dist/manifest.xml", "utf-8");
+    const manifest = parser.parse(manifestRaw);
+    const botVersion = manifest.metadata.bot.version;
+    const warningData =
+      `\n` +
+      "<!-- THIS IS THE METADATA BOT FILE -->" +
+      `\n` +
+      "<!-- Do NOT delete this file or it will crash -->" +
+      `\n` +
+      "<!-- Changes to this file may cause incorrect behavior -->" +
+      `\n` +
+      "<!-- You will be responsible for this when changing any content in the file. -->" +
+      `\n`;
+  
+    manifest.metadata.bot.version = `${botVersion}+${objectDate}`;
+  
+    fse.writeFileSync(
+      "./dist/manifest.xml",
+      builder.build(manifest) + warningData,
+      "utf-8"
+    );
+  
+    logger(
+      "Edit manifest file complete!",
+      "build"
+    );
 
-child.stderr.on("data", (data) => {
-  logger(data, "build");
-});
+    await fse.mkdir("./out");
+    await fse.mkdir("./out/ByteBlaze");
 
-child.on("error", (error) => {
-  logger(error.message, "error");
-});
-
-child.on("close", async (code) => {
-  logger(`Build finished with code ${code}`, "build");
-
-  // Edit manifest
-  const manifestRaw = fse.readFileSync("./dist/manifest.xml", "utf-8");
-  const manifest = parser.parse(manifestRaw);
-  const botVersion = manifest.metadata.bot.version;
-  const warningData =
-    `\n` +
-    "<!-- THIS IS THE METADATA BOT FILE -->" +
-    `\n` +
-    "<!-- Do NOT delete this file or it will crash -->" +
-    `\n` +
-    "<!-- Changes to this file may cause incorrect behavior -->" +
-    `\n` +
-    "<!-- You will be responsible for this when changing any content in the file. -->" +
-    `\n`;
-
-  manifest.metadata.bot.version = `${botVersion}+${objectDate}`;
-
-  fse.writeFileSync(
-    "./dist/manifest.xml",
-    builder.build(manifest) + warningData,
-    "utf-8"
-  );
-
-  logger(
-    "Edit manifest file complete!",
-    "build"
-  );
-
-  // Archive build
-  await fse.mkdir("./out");
-  const path = `./out/ByteBlaze.zip`;
-
-  const ignored = [
-    "node_modules",
-    ".env",
-    ".eslintrc.cjs",
-    ".gitignore",
-    ".prettierrc.json",
-    "app.yml",
-    ".git",
-    ".cylane",
-    "src",
-    "scripts",
-    "build.mjs",
-    "byteblaze.database.json",
-    "pnpm-lock.yaml",
-    "tsconfig.json",
-  ];
-  const zipper = new archiver(".", path, false, ignored);
-  zipper.createZip();
-  logger("Archive all build file successfully!!!", "build");
-  logger("Build bot successfully!!!");
-});
+    copydir.sync('.', './out/ByteBlaze', {
+      filter: function(stat, filepath, filename){
+        if(stat === 'file' && ignored.includes(filename)) {
+          return false;
+        }
+        if (stat === 'directory' && ignored.includes(filename)) {
+          return false;
+        }
+        return true;  // remind to return a true value when file check passed.
+      }
+    });
+  });
+} else {
+  // Build (Local build)
+  const child = spawn(/^win/.test(process.platform) ? "npm.cmd" : "npm", [
+    "run",
+    "build:full",
+  ]);
+  
+  child.stdout.on("data", (data) => {
+    logger(data, "build");
+  });
+  
+  child.stderr.on("data", (data) => {
+    logger(data, "build");
+  });
+  
+  child.on("error", (error) => {
+    logger(error.message, "error");
+  });
+  
+  child.on("close", async (code) => {
+    logger(`Build finished with code ${code}`, "build");
+  
+    // Edit manifest
+    const manifestRaw = fse.readFileSync("./dist/manifest.xml", "utf-8");
+    const manifest = parser.parse(manifestRaw);
+    const botVersion = manifest.metadata.bot.version;
+    const warningData =
+      `\n` +
+      "<!-- THIS IS THE METADATA BOT FILE -->" +
+      `\n` +
+      "<!-- Do NOT delete this file or it will crash -->" +
+      `\n` +
+      "<!-- Changes to this file may cause incorrect behavior -->" +
+      `\n` +
+      "<!-- You will be responsible for this when changing any content in the file. -->" +
+      `\n`;
+  
+    manifest.metadata.bot.version = `${botVersion}+${objectDate}`;
+  
+    fse.writeFileSync(
+      "./dist/manifest.xml",
+      builder.build(manifest) + warningData,
+      "utf-8"
+    );
+  
+    logger(
+      "Edit manifest file complete!",
+      "build"
+    );
+  
+    // Archive build
+    await fse.mkdir("./out");
+    const path = `./out/ByteBlaze.zip`;
+  
+    const zipper = new archiver(".", path, false, ignored);
+    zipper.createZip();
+    logger("Archive all build file successfully!!!", "build");
+    logger("Build bot successfully!!!");
+  });
+}
