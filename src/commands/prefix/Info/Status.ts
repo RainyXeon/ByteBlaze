4c6b2c82d6cb5f9fe1675582d689a49221d1c231
RainyXeon / <minh15052008@gmail.com>
2023-12-06 12:34:19 +0700

    Edit status interface

--- src/commands/prefix/Info/Status.ts
+++ src/commands/prefix/Info/Status.ts
@@ -31,23 +31,23 @@ export default class implements PrefixCommand {
     const used = process.memoryUsage().rss / 1024 / 1024;
 
     const hostInfo = stripIndents`\`\`\`
-    OS: ${os.type()} ${os.release()} (${os.arch()})
-    CPU: ${os.cpus()[0].model}
-    Uptime: ${ms(client.uptime as number)}
-    RAM: ${(total / 1024).toFixed(2)} GB
-    Memory Usage: ${used.toFixed(2)}/${total.toFixed(2)} (MB)
-    Node.js: ${process.version}
+    - OS: ${os.type()} ${os.release()} (${os.arch()})
+    - CPU: ${os.cpus()[0].model}
+    - Uptime: ${ms(client.uptime as number)}
+    - RAM: ${(total / 1024).toFixed(2)} GB
+    - Memory Usage: ${used.toFixed(2)}/${total.toFixed(2)} (MB)
+    - Node.js: ${process.version}
     \`\`\``;
 
     const botInfo = stripIndents`\`\`\`
-    Codename: ${client.metadata.codename}
-    Bot version: ${client.metadata.version}
-    Autofix version: ${client.metadata.autofix}
-    Discord.js: ${version}
-    WebSocket Ping: ${client.ws.ping}ms
-    Response time: ${Date.now() - message.createdTimestamp}ms
-    Guild Count: ${client.guilds.cache.size}
-    User count: ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}
+    - Codename: ${client.metadata.codename}
+    - Bot version: ${client.metadata.version}
+    - Autofix version: ${client.metadata.autofix}
+    - Discord.js: ${version}
+    - WebSocket Ping: ${client.ws.ping}ms
+    - Response time: ${Date.now() - message.createdTimestamp}ms
+    - Guild Count: ${client.guilds.cache.size}
+    - User count: ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}
     \`\`\``;
 
     const embed = new EmbedBuilder()
