9df9213ef2bfca3a2856c8205f81c9e65d96c3ae
RainyXeon / <minh15052008@gmail.com>
2023-12-08 15:34:28 +0700

    Update ready.ts (Will have error because code in phone)

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -4,21 +4,19 @@ export default class {
   async execute(client: Manager) {
     client.logger.info(`Logged in ${client.user!.tag}`);
 
-    const activities = [
+    setInterval(() => {
+      client.user!.setPresence({
+        activities: [
+          {
+            name: `${
+              [
       `with ${client.guilds.cache.size} servers! | /music radio`,
       `with ${client.guilds.cache.reduce(
         (a, b) => a + b.memberCount,
         0
       )} users! | /music play`,
       `with ${client.channels.cache.size} channels! | /filter nightcore`,
-    ];
-
-    setInterval(() => {
-      client.user!.setPresence({
-        activities: [
-          {
-            name: `${
-              activities[Math.floor(Math.random() * activities.length)]
+    ][Math.floor(Math.random() * activities.length)]
             }`,
             type: 2,
           },
