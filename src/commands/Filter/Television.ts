ffebb1bab1f1d3b950abb3552b99a7d0def1b72e
Zoldy⚡ <58603220+ZoldZap@users.noreply.github.com>
2024-01-05 18:59:27 +0600

    Update Television.ts

--- src/commands/Filter/Television.ts
+++ src/commands/Filter/Television.ts
@@ -51,7 +51,7 @@ export default class implements Command {
     const embed = new EmbedBuilder()
       .setDescription(
         `${client.i18n.get(handler.language, "filters", "filter_on", {
-          name: "television",
+          name: "Television",
         })}`
       )
       .setColor(client.color);
