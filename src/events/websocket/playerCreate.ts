b82e74c5c8f6e74e28056446da820526f26f38e2
RainyXeon / <minh15052008@gmail.com>
2024-04-03 12:09:54 +0700

    Clear websocket dir

--- src/events/websocket/playerCreate.ts
+++
@@ -1,8 +0,0 @@
-import { Manager } from "../../manager.js";
-
-export default class {
-  async execute(client: Manager, guildId: string) {
-    if (!client.websocket) return;
-    client.websocket.send(JSON.stringify({ op: "playerCreate", guild: guildId }));
-  }
-}
