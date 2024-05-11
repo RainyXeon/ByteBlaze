e4860fec1d838853ad706d52bc47147e5062c14c
RainyXeon / <minh15052008@gmail.com>
2024-05-11 16:09:48 +0700

    add: owner - admin mode for profile command

--- src/commands/Premium/Profile.ts
+++ src/commands/Premium/Profile.ts
@@ -23,6 +23,9 @@ export default class implements Command {
   public async execute(client: Manager, handler: CommandHandler) {
     await handler.deferReply();
 
+    if (handler.user?.id == client.owner) return this.owner(client, handler);
+    if (client.config.bot.ADMIN.includes(handler.user?.id ?? "null")) return this.admin(client, handler);
+
     const PremiumPlan = (await client.db.premium.get(`${handler.user?.id}`)) as Premium;
     const expires = moment(PremiumPlan && PremiumPlan.expiresAt !== "lifetime" ? PremiumPlan.expiresAt : 0).format(
       "do/MMMM/YYYY (HH:mm:ss)"
@@ -38,6 +41,44 @@ export default class implements Command {
           user: String(handler.user?.tag),
           plan: PremiumPlan!.plan,
           expires: PremiumPlan!.expiresAt == "lifetime" ? "lifetime" : expires,
+        })}`
+      )
+      .setColor(client.color)
+      .setTimestamp();
+
+    return handler.editReply({ embeds: [embed] });
+  }
+
+  owner(client: Manager, handler: CommandHandler) {
+    const embed = new EmbedBuilder()
+      .setAuthor({
+        name: `${client.getString(handler.language, "command.premium", "profile_author")}`,
+        iconURL: client.user!.displayAvatarURL(),
+      })
+      .setDescription(
+        `${client.getString(handler.language, "command.premium", "profile_desc", {
+          user: String(handler.user?.tag),
+          plan: "dreamvast@owner",
+          expires: "lifetime",
+        })}`
+      )
+      .setColor(client.color)
+      .setTimestamp();
+
+    return handler.editReply({ embeds: [embed] });
+  }
+
+  admin(client: Manager, handler: CommandHandler) {
+    const embed = new EmbedBuilder()
+      .setAuthor({
+        name: `${client.getString(handler.language, "command.premium", "profile_author")}`,
+        iconURL: client.user!.displayAvatarURL(),
+      })
+      .setDescription(
+        `${client.getString(handler.language, "command.premium", "profile_desc", {
+          user: String(handler.user?.tag),
+          plan: "dreamvast@admin",
+          expires: "lifetime",
         })}`
       )
       .setColor(client.color)
