8bf07327db01d4ab0f3432650f5a8f3780f4a4b2
RainyXeon / <minh15052008@gmail.com>
2024-05-16 15:33:36 +0700

    rename: all shard event

--- src/events/shard/shardDisconnect.ts
+++ src/events/shard/shardDisconnect.ts
@@ -2,6 +2,6 @@ import { Manager } from "../../manager.js";
 
 export default class {
   async execute(client: Manager, error: Error, id: number) {
-    client.logger.warn("ShardManager", `Shard ${id} Shard Disconnected!`);
+    client.logger.warn("ShardDisconnect", `Shard ${id} Shard Disconnected!`);
   }
 }
